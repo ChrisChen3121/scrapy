316011281481f98c82e46dd3a56659c88673a659
ChrisChen3121 <ChrisChen3121@gmail.com>
2017-11-06 09:50:42 +0800

    change settings

--- yscrapy/yscrapy/settings.py
+++ yscrapy/yscrapy/settings.py
@@ -34,7 +34,8 @@ LOG_LEVEL = 'DEBUG'
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 2
+RANDOMIZE_DOWNLOAD_DELAY = True
+DOWNLOAD_DELAY = 5
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -95,7 +96,7 @@ AUTOTHROTTLE_MAX_DELAY = 60
 # each remote server
 AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-AUTOTHROTTLE_DEBUG = True
+# AUTOTHROTTLE_DEBUG = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
